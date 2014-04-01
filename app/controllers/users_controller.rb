d7850ef22e79648a42a4e778123e40bd595f3888
brabeeba <brabeeba@gmail.com>
2014-04-01 08:44:32 -0400

    horrible bug undetected.....shiiiiit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :signed_in_user, only: [:edit, :update]
+before_action :correct_user,   only: [:edit, :update]
  def show
     @user = User.find(params[:id])
   end
@@ -7,13 +9,26 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-  def create
-    @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
-       flash[:success] = "Welcome to the Peddie Note Sharing World!"
+   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
   
@@ -23,6 +38,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
+     # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
 
