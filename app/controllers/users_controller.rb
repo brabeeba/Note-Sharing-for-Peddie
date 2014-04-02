c0618646f1092934d8a81bed2d5c4b7d217e080c
brabeeba <brabeeba@gmail.com>
2014-04-02 13:43:43 -0400

    still have one failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 before_action :signed_in_user, only: [:edit, :update]
 before_action :correct_user,   only: [:edit, :update]
+  def index
+    @users = User.all
+  end
+
  def show
     @user = User.find(params[:id])
   end
@@ -9,11 +13,12 @@ before_action :correct_user,   only: [:edit, :update]
   	@user = User.new
   end
 
-      def create
-   @user = User.new(user_params)    # Not the final implementation!
-     if @user.save
-        flash[:success] = "Welcome to the Peddie Note Sharing World!"
-       redirect_to @user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -38,6 +43,7 @@ before_action :correct_user,   only: [:edit, :update]
                                    :password_confirmation)
     end
      # Before filters
+
 
     def signed_in_user
       unless signed_in?
--- spec/requests/authtication_pages_spec.rb
+++ spec/requests/authtication_pages_spec.rb
@@ -4,8 +4,6 @@ describe "Authentication" do
 
 	subject { page }
 	  
-
-
 	describe "signin page" do
 		before { visit signin_path }
 
@@ -36,6 +34,7 @@ describe "Authentication" do
 		end
 
 		it { should have_title(user.name) }
+		 it { should have_link('Users',       href: users_path) }
 		it { should have_link('Profile',     href: user_path(user)) }
 		it { should have_link('Sign out',    href: signout_path) }
 		it { should_not have_link('Sign in', href: signin_path) }
@@ -45,7 +44,46 @@ describe "Authentication" do
         it { should have_link('Sign in') }
       end
 	end
-	describe "as wrong user" do
+	
+    describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+       describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+          describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+      end
+    end
+    describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user, no_capybara: true }
@@ -61,5 +99,6 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
+  end
   end
 end
