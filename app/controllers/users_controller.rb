c23d2df0cd06f7668cd4bb169503137c90bde7c0
brabeeba <brabeeba@gmail.com>
2014-04-09 13:45:38 -0400

    creating micropost form with security scheme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ before_action :admin_user, only: :destroy
 
  def show
     @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -50,13 +51,7 @@ before_action :admin_user, only: :destroy
      # Before filters
 
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
+   
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
